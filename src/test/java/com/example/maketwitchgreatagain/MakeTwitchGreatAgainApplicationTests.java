c4bccf717f40045ea728031f58ebb9a7bc8586ce
jderen <kuba35groszy@tlen.pl>
2020-09-05 13:32:01 +0200

    initial commit

---
+++ src/test/java/com/example/maketwitchgreatagain/MakeTwitchGreatAgainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maketwitchgreatagain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeTwitchGreatAgainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
